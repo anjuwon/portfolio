0ac32520eded7502263b6dac5e22ef2033e4805b
anjuwon <83751400+anjuwon@users.noreply.github.com>
2021-06-01 16:35:29 +0900

    index
    업로드

--- js/index.js
+++ js/index.js
@@ -1,33 +1,47 @@
-//풀페이지
-$(function() {
-    $('#fullpage').fullpage({
-        //options here
-        anchors:['section1', 'section2', 'section3', 'section4'],
-        navigation: true,
-        navigationPosition: 'left',
+$(document).ready(function(){
+    //project swiper 
+    var swiper = new Swiper(".explain", {
+        speed: 600,
+        loop: true,
+        allowTouchMove: false,
+        parallax: true,
+        navigation: {
+        nextEl: ".navigation .next",
+        prevEl: ".navigation .prev",
+        },
     });
-});
-
-//project swiper 
-var swiper = new Swiper(".explain", {
-    speed: 600,
-    allowTouchMove: false,
-    loop: true,
-    parallax: true,
-    navigation: {
-        nextEl: ".navigation .next",
-        prevEl: ".navigation .prev",
-    },
-});
-//view swiper
-var swiper = new Swiper(".view, .mobile", {
-    loop: true,
-    allowTouchMove: false,
-    pagination: {
+    //view swiper
+    var swiper = new Swiper(".view", {
+        speed: 600,
+        loop: true,
+        allowTouchMove: false,
+        pagination: {
         el: ".swiper-pagination",
-    },
-    navigation: {
-        nextEl: ".navigation .next",
-        prevEl: ".navigation .prev",
-    },
+        },
+        navigation: {
+            nextEl: ".navigation .next",
+            prevEl: ".navigation .prev",
+        },
+    });
+
+    //스크롤러
+    var s = skrollr.init();
+
+    //AOS
+    AOS.init();
+
+    //content3 버튼 클릭시 애니메이션
+    var bottlePosition = 0;
+    var click = 0;
+    $(".navigation .next").click(function(){
+        while (click < 2){
+            click++;
+            bottlePosition = $(".bottle").animate({left : bottlePosition + "600" + "px"},2000);
+        }
+    });
+
+
+
+
+
 });
