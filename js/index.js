35fe794a927536bb5d1f234297b9864ddb5299fe
anjuwon <83751400+anjuwon@users.noreply.github.com>
2021-07-09 19:29:51 +0900

    up

--- js/index.js
+++ js/index.js
@@ -78,5 +78,46 @@ $(document).ready(function(){
 
 
     
-    //가로스크롤
+    //스크롤값
+    var wscroll = $(window).scrollTop();
+    var dscroll = $(document).scrollTop();
+    var content2 = $("#content2").scrollTop();
+    
+    $(document).scroll(function(){
+        wtop = $(window).scrollTop();
+        dtop = $(document).scrollTop();
+
+        if(dtop >= 1200){
+            $("#content2 .inner").addClass("on");
+        }else {
+            $("#content2 .inner").removeClass("on");
+        }
+
+
+
+        console.log(wtop);
+        console.log(dtop);
+    });
+
+
+    //레이어 팝업
+    // $(".IR").click(function(){
+    //     $("#IR").show();
+    // });
+    // $(".sprite").click(function(){
+    //     $("#sprite").show();
+    // });
+    // $(".slick_js").click(function(){
+    //     $("#slick_js").show();
+    // });
+    // //레이어 팝업 close
+    // $(".close").click(function(e){
+    //     e.preventDefault();
+    //     $("#IR").hide();
+    //     $("#sprite").hide();
+    //     $("#slick_js").hide();
+    // });
+
+
+
 });
