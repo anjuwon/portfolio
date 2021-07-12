a25cee60d19ff5ae5ffe628808b9e2b63986fb79
anjuwon <83751400+anjuwon@users.noreply.github.com>
2021-07-12 20:23:04 +0900

    up

--- js/index.js
+++ js/index.js
@@ -28,7 +28,7 @@ $(document).ready(function(){
     //AOS
     AOS.init();
 
-    //content3 버튼 클릭시 애니메이션
+    //content2 버튼 클릭시 애니메이션
     var Count = 0; 
     var bottlePosition = 0;
     $(".navigation .next").click(function(){
@@ -87,36 +87,17 @@ $(document).ready(function(){
         wtop = $(window).scrollTop();
         dtop = $(document).scrollTop();
 
-        if(dtop >= 1200){
-            $("#content2 .inner").addClass("on");
-        }else {
-            $("#content2 .inner").removeClass("on");
-        }
+        // if(dtop >= 1200){
+        //     $("#content2 .inner").addClass("on");
+        // }else {
+        //     $("#content2 .inner").removeClass("on");
+        // }
 
 
 
         console.log(wtop);
         console.log(dtop);
     });
-
-
-    //레이어 팝업
-    // $(".IR").click(function(){
-    //     $("#IR").show();
-    // });
-    // $(".sprite").click(function(){
-    //     $("#sprite").show();
-    // });
-    // $(".slick_js").click(function(){
-    //     $("#slick_js").show();
-    // });
-    // //레이어 팝업 close
-    // $(".close").click(function(e){
-    //     e.preventDefault();
-    //     $("#IR").hide();
-    //     $("#sprite").hide();
-    //     $("#slick_js").hide();
-    // });
 
 
 
